01efba067ef8a281733035d79d6cd75c41e66dae
azimshaik <cheer.buddu@gmail.com>
2017-01-26 18:16:51 -0500

    Add at head

---
+++ Algorithms/src/algos/Day4_LinkedList.java
@@ -0,0 +1,48 @@
+package algos;
+
+//Add element at Head
+public class Day4_LinkedList {
+	public static Node head = null;
+	int lenght;
+	Day4_LinkedList(){
+		
+	}
+    public class Node {
+    	int data;
+    	Node next;
+    	Node(int data){
+    		this.data = data;
+    		this.next = null;
+    	}
+    	Node(int data, Node next){
+    		this.data = data;
+    		this.next = next;
+    	}
+
+    }
+    public void addAtFirst(int data){
+    	if(head==null){
+    		head = new Node(data);
+    		}else{
+    			Node temp = new Node(data);
+    			temp.next = head;
+        		head = temp;	
+    		}
+    }
+    public static void show(){
+    	Node temp = head;
+    while(temp!=null){
+    	System.out.println(temp.data);
+    	temp = temp.next;
+    }
+    	
+    }
+    public static void main(String[] args) {
+    	Day4_LinkedList ll = new Day4_LinkedList();
+    	ll.addAtFirst(12);
+    	ll.addAtFirst(1278);
+    	ll.addAtFirst(4556);
+    	ll.addAtFirst(67);
+    	ll.show();
+    }
+}
