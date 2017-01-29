1fbf47f597e99eca02ca8e37917924ab1850b3ac
azimshaik <cheer.buddu@gmail.com>
2017-01-28 23:23:40 -0500

    compare to stirng

--- Algorithms/src/algos/Day5_LexicalOrder.java
+++ Algorithms/src/algos/Day5_LexicalOrder.java
@@ -1,5 +1,21 @@
 package algos;
-
+import java.util.Scanner;
 public class Day5_LexicalOrder {
-
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		System.out.println(s1.length()+s2.length());
+		int x = s1.compareTo(s2);
+		String status;
+		if(x<0){
+			status = "Yes";
+		}else if(x>0){
+			status = "No";
+		}else{
+			status = "Lexically Equivalent";
+		}
+		System.out.println(status);
+		
+	}
 }
